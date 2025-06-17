8b815e66825735441ba205e22ece7e16223542ee
KlevertonKulmann1 <kleverton.kulmann1@gmail.com>
2025-06-17 14:17:45 -0300

    feat: added navigation between pages

---
+++ src/pages/Jokes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Jokes: React.FC = () => {
+    return (
+        <div>
+            <h1>Jokes Page</h1>
+            {/* Conteúdo da página de piadas vai aqui */}
+        </div>
+    );
+};
+
+export default Jokes;
