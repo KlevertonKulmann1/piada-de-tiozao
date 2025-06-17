c2557f13421e03334237f100d5f5e2454001540f
KlevertonKulmann1 <kleverton.kulmann1@gmail.com>
2025-06-17 16:14:34 -0300

    fix: width components responsive

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navigation: React.FC<{ pages: Array<{ name: string; label: string, path: string }> }> = ({ pages }) => {
     return (
-        <nav style={{ display: 'flex', gap: '1rem' }}>
+        <nav style={{ display: 'flex', gap: '1rem', width: '100%', justifyContent: 'center', padding: '1rem 0'}}>
         {pages
           .filter(page => page.name.toLowerCase() !== 'home')
           .map(page => (
